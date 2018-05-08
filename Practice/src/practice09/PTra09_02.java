063129a22b6d43cfc6d9ae8628e566e08afbe3f0
SyogoHorita <akifumi232@gmail.com>
2018-05-08 19:02:40 +0900

    10-4

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,4 +1,5 @@
 package practice09;
+import practice09.util.Util;
 /*
  * PTra09_02.java
  *   作成	LIKEIT	2017
@@ -10,6 +11,11 @@ public class PTra09_02 {
 	public static void main(String[] args) {
 		String[] array = {"Plain", "Island", "Mountain", "Swamp", "Forest"};
 
+		String[] s=Util.shuffleArray(array);
+
+		for(int i=0;i<array.length;i++) {
+			System.out.println(s[i]);
+		}
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 
--- Practice/src/practice10/PTra10_04.java
+++ Practice/src/practice10/PTra10_04.java
@@ -15,30 +15,36 @@ public class PTra10_04 {
 	public static void main(String[] args) {
 
 		// ★ User型の変数が3つ入る配列usersを宣言・確保してください
-
+		User[] users=new User[3];
 
 		// ★ java.util.Scannerクラスをインスタンス化し、変数scannerに代入してください
-
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 		// ★ usersのサイズ分繰り返す制御構文を記述してください
 		//----------- ループ start
-
-			System.out.println("##人目の情報を入力します");		/* ##には、カウントを入れてください */
+		for(int i=0;i<users.length;i++) {
+			System.out.println((i+1)+"人目の情報を入力します");		/* ##には、カウントを入れてください */
 			System.out.println("ユーザID（数字）、 ユーザ名、メールアドレス、パスワードをカンマ区切りで入力してください");
 
 			// ★ コマンドプロンプトに入力された値を、String型の変数lineに格納してください
-
+		String line=scanner.nextLine();
 
 			// ★ 変数lineに格納されている値を','で区切り、配列にしてください（Stringクラスのメソッドを使用します）
-
+		String[] list=line.split(",",0);
 
 			// ★ 配列にした値を、users[カウント]の各フィールドに代入してください
 			// ★ ユーザIDに該当する値が数値でなかった場合は、エラーでプログラムが止まっても良いです
-
-
+		users[0].userId=Integer.parseInt(list[0]);
+		users[1].userNm=list[1];
+		users[2].mail=list[2];
+		users[3].password=list[3];
+		}
 		//----------- ループ end
 
 		// ★ usersに格納されている全てのインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		System.out.println(users[0].userId);
+		System.out.println(users[1].userNm);
+		System.out.println(users[2].mail);
+		System.out.println(users[3].password);
 	}
 }
